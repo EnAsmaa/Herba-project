3937bc7265c54ad64adba1bc570d22098c13d897
Abdelrhman <abdelr7mane7ab@gmail.com>
2025-11-18 13:12:22 +0200

    hh

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from "react";
 import style from "../Landing/Landing.module.css";
+import videoBg from '../../assets/lavid.mp4'
 
 export default function Landing() {
   return (
     <div className={`${style.h100}`}>
+        <video
+        className={style.videoBg}
+        src={videoBg}
+        autoPlay
+        loop
+        muted
+        playsInline
+      ></video>
       <div className={`${style.homeContent} text-center`}>
         <h1 className={`text-white ${style.h1s}`}>
           Rediscover Your <br /> Balance, Naturally.
