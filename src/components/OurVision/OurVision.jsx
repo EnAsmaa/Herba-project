2f8fe99c4da6026c47ee19b59485b941ba2ae698
Asmaa Elnger <asmaaelnger02@gmail.com>
2025-11-25 05:11:40 +0200

    updates

--- src/components/OurVision/OurVision.jsx
+++ src/components/OurVision/OurVision.jsx
@@ -5,51 +5,53 @@ import OurVision3 from "../../assets/pexels-mareefe-672046.jpg";
 
 export default function OurVision() {
   return (
-    <div data-aos="" className="">
-      <h2 className="text-center font-bold text-3xl">Our Vision</h2>
-      <p className="text-center px-md-5 mx-md-5">
-        Our vision is to become a leading source of natural wellness, empowering
-        people to embrace a healthier lifestyle through the gentle power of
-        herbs.
-      </p>
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-3 mt-5">
-        <div className="vision-col-content text-center">
-          <img
-            className="w-full rounded-md mb-3"
-            src={OurVision1}
-            alt={OurVision1}
-          />
-          <h4>Natural Ingredients</h4>
-          <p>
-            We source herbs directly from nature to ensure purity,
-            freshness, and real health benefits.
-          </p>
-        </div>
-        <div className="vision-col-content text-center">
-          <img
-            className="w-full rounded-md mb-3"
-            src={OurVision3}
-            alt={OurVision3}
-          />
-          <h4>Eco-Friendly</h4>
-          <p>
-            Our products and packaging are designed with the planet in mind
-            — sustainable and safe.
-          </p>
-        </div>
-        <div className="vision-col-content text-center">
-          <img
-            className="w-full rounded-md mb-3"
-            src={OurVision2}
-            alt={OurVision2}
-          />
-          <h4>Trusted Quality</h4>
-          <p>
-            Each product is crafted with care and tested for quality to
-            support your well-being.
-          </p>
+    <section className="bg-[#e6f0ea] dark:bg-[#111B21] py-10">
+      <div className="container mx-auto px-4 py-2">
+        <h2 className="text-center font-bold text-3xl mb-5 dark:text-[#E9EDEF]">Our Vision</h2>
+        <p className="text-center text-lg text-[#333333] dark:text-[#C5CFD6] md:w-3/4 mx-auto">
+          Our vision is to become a leading source of natural wellness,
+          empowering people to embrace a healthier lifestyle through the gentle
+          power of herbs.
+        </p>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-5 mt-6">
+          <div className="vision-col-content text-center">
+            <img
+              className="w-full rounded-xl mb-3"
+              src={OurVision1}
+              alt={OurVision1}
+            />
+            <h3 className="text-[#203825] dark:text-[#4d805e] text-xl font-medium">Natural Ingredients</h3>
+            <p className="text-base text-[#555555] dark:text-[#C5CFD6]">
+              We source herbs directly from nature to ensure purity, freshness,
+              and real health benefits.
+            </p>
+          </div>
+          <div className="vision-col-content text-center">
+            <img
+              className="w-full rounded-xl mb-3"
+              src={OurVision3}
+              alt={OurVision3}
+            />
+            <h3 className="text-[#203825] dark:text-[#4d805e] text-xl font-medium">Eco-Friendly</h3>
+            <p className="text-base text-[#555555] dark:text-[#C5CFD6]">
+              Our products and packaging are designed with the planet in mind —
+              sustainable and safe.
+            </p>
+          </div>
+          <div className="vision-col-content text-center">
+            <img
+              className="w-full rounded-xl mb-3"
+              src={OurVision2}
+              alt={OurVision2}
+            />
+            <h3 className="text-[#203825] text-xl font-medium dark:text-[#4d805e]">Trusted Quality</h3>
+            <p className="text-base text-[#555555] dark:text-[#C5CFD6]" >
+              Each product is crafted with care and tested for quality to
+              support your well-being.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
--- src/components/Testmonials/Testmonials.jsx
+++ src/components/Testmonials/Testmonials.jsx
@@ -1,82 +1,86 @@
-import { FaRegStar} from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
 import person1 from "../../assets/person1.jpg";
 import person2 from "../../assets/person2.jpg";
 import person3 from "../../assets/person3.jpg";
 export default function Testmonials() {
   return (
-    <div data-aos="" className="">
-      <h2 className="text-center font-bold text-3xl">What Our Community Says</h2>
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-5 mt-5">
-        <div className="testmonial-content border rounded-md p-3">
-          <div className="testmonial-person mb-3 flex items-center gap-3">
-            <img
-              src={person1}
-              className="aspect-square w-15 rounded-full object-cover"
-              alt="testimonial"
-            />
-            <div>
-              <span className="fw-bold">Mariam S.</span>
-              <div className="star-icons flex gap-2 mt-2 items-center justify-center">
-                <FaRegStar className="text-yellow-400" />
-                <FaRegStar className="text-yellow-400" />
-                <FaRegStar className="text-yellow-400" />
-                <FaRegStar className="text-yellow-400" />
+    <section className="pb-10 mb-10 ">
+      <div className="container mx-auto px-4">
+        <h2 className="text-center font-bold text-3xl mb-7 dark:text-[#E9EDEF]">
+          What Our Community Says
+        </h2>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-5 mt-5">
+          <div className="testmonial-content border border-[#adabab] dark:border-[#294353a6] rounded-xl p-3 ps-4">
+            <div className="testmonial-person mb-3 flex items-center gap-5">
+              <img
+                src={person1}
+                className="aspect-square w-15 rounded-full object-cover"
+                alt="testimonial"
+              />
+              <div>
+                <span className="font-semibold text-[#4d7c5c]">Mariam S.</span>
+                <div className="star-icons flex gap-2 mt-2 items-center justify-center">
+                  <FaRegStar className="text-yellow-400" />
+                  <FaRegStar className="text-yellow-400" />
+                  <FaRegStar className="text-yellow-400" />
+                  <FaRegStar className="text-yellow-400" />
+                </div>
               </div>
             </div>
+            <p className="text-[#333333] dark:text-[#8696A0]">
+              “HerbalCare completely changed my skincare routine! I love knowing
+              that everything I use is 100% natural and gentle on my skin. My
+              complexion has never looked this healthy.”
+            </p>
           </div>
-          <p >
-            “HerbalCare completely changed my skincare routine! I love
-            knowing that everything I use is 100% natural and gentle on my
-            skin. My complexion has never looked this healthy.”
-          </p>
-        </div>
-        <div className="testmonial-content border rounded-md p-3">
-          <div className="testmonial-person mb-3 flex items-center gap-3">
-            <img
-              src={person3}
-              className="aspect-square w-15 rounded-full object-cover"
-              alt="testimonial"
-            />
-            <div>
-              <span className="fw-bold">Omar M.</span>
-              <div className="star-icons flex gap-2 mt-2 items-center justify-center">
-                <FaRegStar className="text-yellow-400" />
-                <FaRegStar className="text-yellow-400" />
-                <FaRegStar className="text-yellow-400" />
-                <FaRegStar className="text-yellow-400" />
+          <div className="testmonial-content border dark:border-[#294353a6] border-[#adabab] rounded-xl p-3 ps-4">
+            <div className="testmonial-person mb-3 flex items-center gap-3">
+              <img
+                src={person3}
+                className="aspect-square w-15 rounded-full object-cover"
+                alt="testimonial"
+              />
+              <div>
+                <span className="font-semibold text-[#4d7c5c]">Omar M.</span>
+                <div className="star-icons flex gap-2 mt-2 items-center justify-center">
+                  <FaRegStar className="text-yellow-400" />
+                  <FaRegStar className="text-yellow-400" />
+                  <FaRegStar className="text-yellow-400" />
+                  <FaRegStar className="text-yellow-400" />
+                </div>
               </div>
             </div>
+            <p className="text-[#333333] dark:text-[#8696A0]">
+              “I started using HerbalCare’s herbal teas a few months ago, and
+              they’ve helped me feel more relaxed and energized. It’s amazing
+              how nature can make such a difference!”
+            </p>
           </div>
-          <p>
-            “I started using HerbalCare’s herbal teas a few months ago, and
-            they’ve helped me feel more relaxed and energized. It’s amazing
-            how nature can make such a difference!”
-          </p>
-        </div>
-        <div className="testmonial-content border rounded-md p-3">
-          <div className="testmonial-person  mb-3 flex items-center gap-3">
-            <img
-              src={person2}
-              className="aspect-square w-15 rounded-full object-cover"
-              alt="testimonial"
-            />
-            <div>
-              <span className="fw-bold">Nour H.</span>
-              <div className="star-icons flex gap-2 mt-2 items-center justify-center">
-                <FaRegStar className="text-yellow-400" />
-                <FaRegStar className="text-yellow-400" />
-                <FaRegStar className="text-yellow-400" />
-                <FaRegStar className="text-yellow-400" />
+          <div className="testmonial-content border dark:border-[#294353a6] border-[#adabab] rounded-xl p-3 ps-4">
+            <div className="testmonial-person  mb-3 flex items-center gap-3">
+              <img
+                src={person2}
+                className="aspect-square w-15 rounded-full object-cover"
+                alt="testimonial"
+              />
+              <div>
+                <span className="font-semibold text-[#4d7c5c]">Nour H.</span>
+                <div className="star-icons flex gap-2 mt-2 items-center justify-center">
+                  <FaRegStar className="text-yellow-400" />
+                  <FaRegStar className="text-yellow-400" />
+                  <FaRegStar className="text-yellow-400" />
+                  <FaRegStar className="text-yellow-400" />
+                </div>
               </div>
             </div>
+            <p className="text-[#333333] dark:text-[#8696A0]">
+              “I started using HerbalCare’s herbal teas a few months ago, and
+              they’ve helped me feel more relaxed and energized. It’s amazing
+              how nature can make such a difference!”
+            </p>
           </div>
-          <p>
-            “I started using HerbalCare’s herbal teas a few months ago, and
-            they’ve helped me feel more relaxed and energized. It’s amazing
-            how nature can make such a difference!”
-          </p>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
