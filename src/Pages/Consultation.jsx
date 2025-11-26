51f95e7f5b0188a8430ed9878c2bf8d2c55b150d
Asmaa Elnger <asmaaelnger02@gmail.com>
2025-11-26 15:09:33 +0200

    uuu

--- src/Pages/Consultation.jsx
+++ src/Pages/Consultation.jsx
@@ -1,9 +1,9 @@
 import { FaRegStar } from "react-icons/fa";
-import { FaSearch } from "react-icons/fa";
 import person1 from "../assets/person1.jpg";
 import person4 from "../assets/person4.jpg";
 import person2 from "../assets/person2.jpg";
 import person3 from "../assets/person3.jpg";
+import Search from "../components/Search";
 export default function Consultation() {
   return (
     <section className="my-9">
@@ -16,17 +16,8 @@ export default function Consultation() {
             Book your session with the best certified consultants
           </p>
         </div>
-
-        <div className="search-wrapper bg-green-800/10 py-8 rounded-lg">
-          <div className="search-box relative mx-auto w-2xs sm:w-sm md:w-md lg:w-lg">
-            <FaSearch className="absolute text-gray-500 -translate-y-1/2 top-1/2 left-3 cursor-pointer fs-4" />
-            <input
-              type="search"
-              className="placeholder:text-gray-500 dark:placeholder:text-[#8696A0] placeholder:italic bg-gray-50 text-gray-900 border border-gray-300 dark:border-[#2E3B42] dark:bg-[#1F2C32] dark:text-white focus:outline-0 rounded-lg py-2 px-8 w-full "
-              placeholder=" Search for Doctors..."
-            />
-          </div>
-        </div>
+        {/* search */}
+        <Search placehoder={'doctors'}/>
 
       </div>
       <div className="container mx-auto px-4 py-10 my-5">
